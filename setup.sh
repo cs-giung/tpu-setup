48a13c7d1aeb592c4e02d9049a9b5253976fe2af
cs-giung <50647481+cs-giung@users.noreply.github.com>
2024-04-13 11:36:26 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ sudo apt-get install -y -qq golang neofetch zsh mosh byobu aria2
 # Change timezone
 sudo timedatectl set-timezone Asia/Seoul
 
-# Install Python 3.10
+# Install Python 3.12
 sudo apt-get install -y -qq software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y -qq python3.10-full python3.10-dev
+sudo apt-get install -y -qq python3.12-full python3.12-dev
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
